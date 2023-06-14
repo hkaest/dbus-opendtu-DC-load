6757926b44f4cd40b83b5ed2a38e22b74c54c8dd
hkaest <136511056+hkaest@users.noreply.github.com>
2023-06-14 21:04:37 +0200

    Update dbus_service.py

--- dbus_service.py
+++ dbus_service.py
@@ -111,13 +111,13 @@ class DbusService:
         logging.info(f"Name of Inverters found: {self._get_name()}")
         self._dbusservice.add_path("/Connected", 1)
 
-        # self._dbusservice.add_path("/Latency", None)
-        # self._dbusservice.add_path("/FirmwareVersion", 0.1)
-        # self._dbusservice.add_path("/HardwareVersion", 0)
-        # self._dbusservice.add_path("/Position", self.acposition)  # normaly only needed for pvinverter
-        # self._dbusservice.add_path("/Serial", self._get_serial(self.pvinverternumber))
-        # self._dbusservice.add_path("/UpdateIndex", 0)
-        # self._dbusservice.add_path("/StatusCode", 0)  # Dummy path so VRM detects us as a PV-inverter.
+        self._dbusservice.add_path("/Latency", None)
+        self._dbusservice.add_path("/FirmwareVersion", 0.1)
+        self._dbusservice.add_path("/HardwareVersion", 0)
+        self._dbusservice.add_path("/Position", self.acposition)  # normaly only needed for pvinverter
+        self._dbusservice.add_path("/Serial", self._get_serial(self.pvinverternumber))
+        self._dbusservice.add_path("/UpdateIndex", 0)
+        self._dbusservice.add_path("/StatusCode", 0)  # Dummy path so VRM detects us as a PV-inverter.
 
         # If the Servicname is an (AC-)Inverter, add the Mode path (to show it as ON)
         # Also, we will set different paths and variables in the _update(self) method.
