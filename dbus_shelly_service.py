7f2aef10d3e06631b2601465e33d248d209b5a6b
hkaest <136511056+hkaest@users.noreply.github.com>
2024-02-24 22:54:37 +0100

    Fixed last commit

--- dbus_shelly_service.py
+++ dbus_shelly_service.py
@@ -123,7 +123,7 @@ class DbusShellyemService:
 
         # add listener to HM micro inverter energy meter, EM111 as Modbus #1
         if 'com.victronenergy.acload.cgwacs_ttyUSB0_mb1' in dbus_conn.list_names():
-            self._HM_meter = VeDbusItemImport(dbus_conn, ' ', '/Ac/L1/Power', self._callback_HM_Power)
+            self._HM_meter = VeDbusItemImport(dbus_conn, 'com.victronenergy.acload.cgwacs_ttyUSB0_mb1', '/Ac/L1/Power', self._callback_HM_Power)
 
         # add listener to SOC
         if 'com.victronenergy.battery.socketcan_can0' in dbus_conn.list_names():
